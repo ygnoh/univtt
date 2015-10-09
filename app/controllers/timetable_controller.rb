60001d7a8768a2e92a6eb56c7b9172f084d2bf65
nohyonggoo <yong115_@naver.com>
2015-10-09 17:52:46 +0900

    Form temporary structure about lecture data with timetable setting

--- app/controllers/timetable_controller.rb
+++ app/controllers/timetable_controller.rb
@@ -3,6 +3,7 @@ class TimetableController < ApplicationController
   end
 
   def new
+		@samples = [{lecture_name: "선형대수1", time: [1000,1300], day: ["wed"], prof_name: "민강주"}, {lecture_name: "선형대수2", time: [1200,1330], day: ["fri"], prof_name: "안재현"}]
   end
 
   def create
