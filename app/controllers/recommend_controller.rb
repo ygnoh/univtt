f8f6de575807fcf733e36515f9bad537e1702eef
nohyonggoo <yong115_@naver.com>
2015-11-20 16:01:38 +0900

    Make overlapChecker in recommend controller

--- app/controllers/recommend_controller.rb
+++ app/controllers/recommend_controller.rb
@@ -4,7 +4,29 @@ class RecommendController < ApplicationController
   end
 
   def result
-		@lectures = params[:lecture_id].split(',').map(&:to_i) # convert string to array
+		lectures = params[:lecture_id].split(',').map(&:to_i) # convert string to array
+		overlapChecker = Hash.new{ |hash, key| hash[key] = [] }
+
+		for i in 0..(lectures.length-1-1) # length <=2 인 경우 따로해야 함
+			Lecturetime.where(lecture_id: lectures[i]).each do |li|
+				for j in (i+1)..(lectures.length-1)
+					if overlapChecker[lectures[i]].include? lectures[j]
+						next
+					end
+
+					Lecturetime.where(lecture_id: lectures[j]).each do |lj|
+						if li.day == lj.day
+							# Check if there exists overlap between li and lj
+							if (lj.starttime < li.starttime && li.starttime < lj.endtime) || (lj.starttime < li.endtime && li.endtime < lj.endtime) || (li.starttime < lj.starttime && lj.starttime < li.endtime) || (li.starttime < lj.endtime && lj.endtime < li.endtime) || (li.starttime == lj.starttime && li.endtime == lj.endtime)
+								overlapChecker[lectures[i]] << lectures[j]
+								overlapChecker[lectures[j]] << lectures[i]
+								break
+							end
+						end
+					end
+				end
+			end
+		end
   end
 
   def create
