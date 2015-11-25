b193ea6b5d460af204c7ea937992082192ea519d
nohyonggoo <yong115_@naver.com>
2015-11-25 18:01:17 +0900

    Intergrate lecture cases

--- app/controllers/recommend_controller.rb
+++ app/controllers/recommend_controller.rb
@@ -8,49 +8,54 @@ class RecommendController < ApplicationController
 		overlapChecker = Hash.new{ |hash, key| hash[key] = [] }
 
 		if lectures.length == 1
-			@result = [lectures] and return
-		end
-		if lectures.length == 2
+			@result = [lectures]
+		elsif lectures.length == 2
+			done = false # for checking if @result exists in length=2
 			Lecturetime.where(lecture_id: lectures[0]).each do |l0|
-				Lecturetime.where(lecture_id: lectures[1]).each do |l1|
-					if l0.day == l1.day && ( (l0.starttime < l1.starttime && l1.starttime < l0.endtime) || (l0.starttime < l1.endtime && l1.endtime < l0.endtime) || (l1.starttime < l0.starttime && l0.starttime < l1.endtime) || (l1.starttime < l0.endtime && l0.endtime < l1.endtime) || (l0.starttime == l1.starttime && l0.endtime == l1.endtime) )
-						@result = [ [lectures[0]], [lectures[1]] ] and return
+				if !done
+					Lecturetime.where(lecture_id: lectures[1]).each do |l1|
+						if l0.day == l1.day && ( (l0.starttime < l1.starttime && l1.starttime < l0.endtime) || (l0.starttime < l1.endtime && l1.endtime < l0.endtime) || (l1.starttime < l0.starttime && l0.starttime < l1.endtime) || (l1.starttime < l0.endtime && l0.endtime < l1.endtime) || (l0.starttime == l1.starttime && l0.endtime == l1.endtime) )
+							@result = [ [lectures[0]], [lectures[1]] ]
+							done = true
+						end
 					end
 				end
 			end
-			@result = [ [lectures[0], lectures[1]] ] and return
-		end
+			if !done
+				@result = [ [lectures[0], lectures[1]] ]
+			end
+		else
+			for i in 0..(lectures.length-1-1)
+				Lecturetime.where(lecture_id: lectures[i]).each do |li|
+					for j in (i+1)..(lectures.length-1)
+						if overlapChecker[lectures[i]].include? lectures[j]
+							next
+						end
 
-		for i in 0..(lectures.length-1-1)
-			Lecturetime.where(lecture_id: lectures[i]).each do |li|
-				for j in (i+1)..(lectures.length-1)
-					if overlapChecker[lectures[i]].include? lectures[j]
-						next
-					end
-
-					Lecturetime.where(lecture_id: lectures[j]).each do |lj|
-						if li.day == lj.day
-							# Check if there exists overlap between li and lj
-							if (lj.starttime < li.starttime && li.starttime < lj.endtime) || (lj.starttime < li.endtime && li.endtime < lj.endtime) || (li.starttime < lj.starttime && lj.starttime < li.endtime) || (li.starttime < lj.endtime && lj.endtime < li.endtime) || (li.starttime == lj.starttime && li.endtime == lj.endtime)
-								overlapChecker[lectures[i]] << lectures[j]
-								overlapChecker[lectures[j]] << lectures[i]
-								break
+						Lecturetime.where(lecture_id: lectures[j]).each do |lj|
+							if li.day == lj.day
+								# Check if there exists overlap between li and lj
+								if (lj.starttime < li.starttime && li.starttime < lj.endtime) || (lj.starttime < li.endtime && li.endtime < lj.endtime) || (li.starttime < lj.starttime && lj.starttime < li.endtime) || (li.starttime < lj.endtime && lj.endtime < li.endtime) || (li.starttime == lj.starttime && li.endtime == lj.endtime)
+									overlapChecker[lectures[i]] << lectures[j]
+									overlapChecker[lectures[j]] << lectures[i]
+									break
+								end
 							end
 						end
 					end
 				end
 			end
-		end
 
-		@result = []
-		lectures.each do |l|
-			@result += recommend([l],lectures,overlapChecker)
+			@result = []
+			lectures.each do |l|
+				@result += recommend([l],lectures,overlapChecker)
+			end
+			@result.uniq!.each do |r|
+				r.sort!
+			end
+			@result = @result.uniq.sort { |x,y| y.length <=> x.length }
+			#@checker = overlapChecker
 		end
-		@result.uniq!.each do |r|
-			r.sort!
-		end
-		@result = @result.uniq.sort { |x,y| y.length <=> x.length }
-		#@checker = overlapChecker
   end
 
 	def recommend(mustVal, array, checker)
