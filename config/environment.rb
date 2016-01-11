db4662119b36b9e46d23a8057b2dac7fce2311d6
EC2 Default User <ec2-user@ip-172-31-26-199.ap-northeast-1.compute.internal>
2016-01-11 11:42:58 +0000

    Edit environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,9 @@
+# Load environment variables
+environment_variables = File.expand_path('./environment_variables.rb', __FILE__)
+load(environment_variables) if File.exists?(environment_variables)
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
-# Load environment variables
-environment_variables = File.join(Rails.root, 'config', 'environment_variables.rb')
-load(environment_variables) if File.exists?(environment_variables)
 
 # Initialize the Rails application.
 Rails.application.initialize!
