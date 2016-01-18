5d2e9c7c241983fb4e991ef1cdbae792a9173d61
yonggoonoh <nohyg90@gmail.com>
2016-01-18 15:24:10 +0900

    Complete connection

---
+++ config/environment_variables.rb
@@ -0,0 +1,11 @@
+# RDS
+ENV['RDS_USERNAME'] = 'devunivtt'
+ENV['RDS_PASSWORD'] = 'startup2015'
+ENV['RDS_HOSTNAME'] = 'univtt-db.chl0wmdsqtty.ap-northeast-1.rds.amazonaws.com'
+ENV['RDS_PORT'] = '5432'
+
+if Rails.env.development?
+  ENV['RDS_DB_NAME'] = 'univtt_development'
+elsif Rails.env.production?
+  ENV['RDS_DB_NAME'] = 'univtt_production'
+end
