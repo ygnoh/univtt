31dbbbf811c6887c3fbcb3b6fc4851cc6aaf021a
yonggoonoh <nohyg90@gmail.com>
2016-01-20 16:36:29 +0900

    Complete connection

--- db/schema.rb
+++ db/schema.rb
@@ -13,64 +13,61 @@
 
 ActiveRecord::Schema.define(version: 20151128211303) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "buildings", force: :cascade do |t|
-    t.integer  "school_id"
-    t.string   "building_name"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.boolean  "active",        default: true
+    t.integer  "school_id",     limit: 4
+    t.string   "building_name", limit: 255
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.boolean  "active",                    default: true
   end
 
   add_index "buildings", ["school_id"], name: "index_buildings_on_school_id", using: :btree
 
   create_table "classifications", force: :cascade do |t|
-    t.integer  "school_id"
-    t.string   "classification_name"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.integer  "school_id",           limit: 4
+    t.string   "classification_name", limit: 255
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
   add_index "classifications", ["school_id"], name: "index_classifications_on_school_id", using: :btree
 
   create_table "classrooms", force: :cascade do |t|
-    t.integer  "building_id"
-    t.string   "classroom_name"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.integer  "building_id",    limit: 4
+    t.string   "classroom_name", limit: 255
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   add_index "classrooms", ["building_id"], name: "index_classrooms_on_building_id", using: :btree
 
   create_table "departments", force: :cascade do |t|
-    t.integer  "school_id"
-    t.string   "department_name"
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.boolean  "active",          default: true
+    t.integer  "school_id",       limit: 4
+    t.string   "department_name", limit: 255
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.boolean  "active",                      default: true
   end
 
   add_index "departments", ["school_id"], name: "index_departments_on_school_id", using: :btree
 
   create_table "lectures", force: :cascade do |t|
-    t.integer  "department_id"
-    t.integer  "professor_id"
-    t.integer  "classification_id"
-    t.integer  "year"
-    t.integer  "semester"
-    t.string   "lecture_name"
-    t.integer  "level"
-    t.integer  "lecture_number"
-    t.string   "lecture_division"
-    t.integer  "grade"
-    t.boolean  "relative",          default: true
-    t.boolean  "passfail",          default: false
-    t.boolean  "english",           default: false
-    t.boolean  "active",            default: true
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.integer  "department_id",     limit: 4
+    t.integer  "professor_id",      limit: 4
+    t.integer  "classification_id", limit: 4
+    t.integer  "year",              limit: 4
+    t.integer  "semester",          limit: 4
+    t.string   "lecture_name",      limit: 255
+    t.integer  "level",             limit: 4
+    t.integer  "lecture_number",    limit: 4
+    t.string   "lecture_division",  limit: 255
+    t.integer  "grade",             limit: 4
+    t.boolean  "relative",                      default: true
+    t.boolean  "passfail",                      default: false
+    t.boolean  "english",                       default: false
+    t.boolean  "active",                        default: true
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
   end
 
   add_index "lectures", ["classification_id"], name: "index_lectures_on_classification_id", using: :btree
@@ -78,60 +75,60 @@ ActiveRecord::Schema.define(version: 20151128211303) do
   add_index "lectures", ["professor_id"], name: "index_lectures_on_professor_id", using: :btree
 
   create_table "lecturetimes", force: :cascade do |t|
-    t.integer  "lecture_id"
-    t.integer  "classroom_id"
-    t.integer  "day"
-    t.integer  "starttime"
-    t.integer  "endtime"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer  "lecture_id",   limit: 4
+    t.integer  "classroom_id", limit: 4
+    t.integer  "day",          limit: 4
+    t.integer  "starttime",    limit: 4
+    t.integer  "endtime",      limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "lecturetimes", ["classroom_id"], name: "index_lecturetimes_on_classroom_id", using: :btree
   add_index "lecturetimes", ["lecture_id"], name: "index_lecturetimes_on_lecture_id", using: :btree
 
   create_table "professors", force: :cascade do |t|
-    t.integer  "department_id"
-    t.string   "professor_name"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.integer  "department_id",  limit: 4
+    t.string   "professor_name", limit: 255
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   add_index "professors", ["department_id"], name: "index_professors_on_department_id", using: :btree
 
   create_table "savetimetables", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "lectures",   default: [],                array: true
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.boolean  "active",     default: true
+    t.integer  "user_id",    limit: 4
+    t.string   "lectures",   limit: 255, default: "--- []\n"
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
+    t.boolean  "active",                 default: true
   end
 
   add_index "savetimetables", ["user_id"], name: "index_savetimetables_on_user_id", using: :btree
 
   create_table "schools", force: :cascade do |t|
-    t.string   "school_name"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.boolean  "active",      default: true
+    t.string   "school_name", limit: 255
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "active",                  default: true
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "username",               default: "",    null: false
-    t.boolean  "admin",                  default: false
-    t.boolean  "active",                 default: true
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "username",               limit: 255, default: "",    null: false
+    t.boolean  "admin",                              default: false
+    t.boolean  "active",                             default: true
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
